5393df0da646593f06aba5f6c73154928e600ff0
Advaitva Gupta <43316329+Advaitva@users.noreply.github.com>
2026-08-18 16:22:51 +0530

    Change SHEET_API_URL to live deployment URL
    Updated SHEET_API_URL to use the actual Google Apps Script deployment URL.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Multi-Channel Recommendation Bot")
 # CONFIGURATION & ENVIRONMENT VARIABLES
 # ---------------------------------------------------------
 # NOTE: This MUST be the Google Apps Script Web App URL ending in /exec
-SHEET_API_URL = os.getenv("SHEET_API_URL", "YOUR_GOOGLE_APPS_SCRIPT_DEPLOYMENT_URL")
+SHEET_API_URL = os.getenv("SHEET_API_URL", "https://script.google.com/macros/s/AKfycbx-loBhWQjC-B84Jj8Fl53WaciOK_VfeFjDXywoNAbmmvX_yAFH8vbgBE_8GRcxflSYzA/exec")
 
 # Telegram Config
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8811090240:AAFNMoQKp3h99xVFQhbK9-MqV1rSsEIO2Ig")
